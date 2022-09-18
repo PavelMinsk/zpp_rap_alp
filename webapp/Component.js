e891fe93a98d1ae258afb8280aaeb643836a6550
Pavel Pinchuk <pvlmby@gmail.com>
2022-09-18 11:19:08 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("alpnspace.alp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
